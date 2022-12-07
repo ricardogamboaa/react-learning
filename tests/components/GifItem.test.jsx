a1f7932fdcbc62a4ff34f7d294468d1f740ed38f
Ricardo Gamboa <ricardogamboa@live.com>
2022-12-07 14:30:43 -0600

    changes on gifItem and its snapshot

--- tests/components/GifItem.test.jsx
+++ tests/components/GifItem.test.jsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { GifItem } from "../../src/components/GifItem"
 
 
@@ -7,12 +7,27 @@ describe('gif item component tests', () => {
     
     const title = "My Title"
     const url = "https://media0.giphy.com/media/DuVRadBbaX6A8/giphy.gif?cid=071c1690ixpua1kwp4zpbhimm1ir5dkao6rvadzbf2twwg4r&rid=giphy.gif&ct=g";
-
+    
     test('should evaluate against snapshot', () => { 
         
         const { container } = render( <GifItem 
-                                        title={ title }
-                                        url={ url } /> );        
-        expect( container ).toMatchSnapshot();
-     });
- });
+            title={ title }
+            url={ url } /> );        
+            expect( container ).toMatchSnapshot();
+        });
+        
+        test('should show the image with the url and alt indicated', () => { 
+            render(<GifItem title={ title } url={ url } />);
+            // screen.debug();
+
+            const { src, alt } = screen.getByRole( 'img' ); 
+            expect( src ).toBe( url );
+            expect( alt ).toBe( title );
+        });
+
+        test('should show title in the component', () => { 
+            render(<GifItem title={ title } url={ url } />);
+
+            expect( screen.getByText( title ) );
+         })
+    });
